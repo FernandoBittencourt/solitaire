7a404904805ce437e0bb1ae5aeac925999733b86
FernandoBittencourt <fernandodbittencourt@hotmail.com>
2026-03-01 15:32:09 -0300

    add deckservice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import solitaire.domain.Card;
+import solitaire.domain.Suit;
+import solitaire.service.DeckService;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+        var deckService = new DeckService(12L);
+        var cardArray = deckService.generate();
+
+        for(var card: cardArray){
+            System.out.println(card.getSymbol());
+        }
     }
 }
---
+++ src/solitaire/service/DeckService.java
@@ -0,0 +1,40 @@
+package solitaire.service;
+
+import solitaire.domain.Card;
+import solitaire.domain.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Collections.shuffle;
+
+public class DeckService {
+
+    private final Random random;
+
+
+    public DeckService(long seed){
+        random=new Random(seed);
+    }
+
+    public List<Card> generate(){
+        List<Card> deck = create();
+        shuffle(deck);
+        return deck;
+    }
+
+    private List<Card> create(){
+        List<Card> deck = new ArrayList<>();
+        for(var suit: Suit.values()) {
+            for(int i=1;i<=13;i++){
+                deck.add(new Card(i, suit));
+            }
+        }
+        return deck;
+    }
+
+    private void shuffle(List<Card> deck){
+
+    }
+}
